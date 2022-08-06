cf909c5553ae0d3e4a04626b909b19304b4dfec9
jessw4914 <jessica.workman76@gmail.com>
2022-08-06 11:05:50 -0500

    sttart of postgres

---
+++ data/scripts/8_6_22.sql
@@ -0,0 +1,27 @@
+
+/* number 1*/
+/*select count(*) from data_analyst_jobs;*/
+
+
+/* number 2*/
+
+/*select *
+from data_analyst_jobs
+limit 11;*/
+
+/* number 3
+21*/
+select count(*)
+from data_analyst_jobs
+where location = 'TN' 
+
+select count(*)
+from data_analyst_jobs
+where location = 'TN' 
+
+/* number 4
+27*/
+
+select count(*)
+from data_analyst_jobs
+where location = 'KY' OR  LOCATION ='TN';
